a2842e4b27c51af4048357a6ab05cbcccfa3c820
Alex <komarevich.alex@gmail.com>
2019-06-24 22:57:07 +0300

    Updates

---
+++ precache-manifest.b1ed9e9cb4945b836ddd4ae5895a1e1d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "09bc231d70aca7374053e807618f4a58",
+    "url": "/jonBlogTest/index.html"
+  },
+  {
+    "revision": "09c15f4a3dd2b4cf2a4a",
+    "url": "/jonBlogTest/static/css/main.5761b056.chunk.css"
+  },
+  {
+    "revision": "45e7b6eed978d7f07d11",
+    "url": "/jonBlogTest/static/js/2.4bde153e.chunk.js"
+  },
+  {
+    "revision": "09c15f4a3dd2b4cf2a4a",
+    "url": "/jonBlogTest/static/js/main.203375c1.chunk.js"
+  },
+  {
+    "revision": "6acb83060419e35f9cb7",
+    "url": "/jonBlogTest/static/js/runtime~main.ceb0d603.js"
+  }
+]);
